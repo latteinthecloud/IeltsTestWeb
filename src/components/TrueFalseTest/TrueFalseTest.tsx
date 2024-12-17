f3cbebcb8d97e27be0335b498659ae07bdf01965
Test_repo <22520992@gm.uit.edu.vn>
2024-12-17 15:55:08 +0700

    Add true/false, matching question

---
+++ src/components/TrueFalseTest/TrueFalseTest.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TrueFalseTest.css"
+
+export default function TrueFalseTest(){
+    return (
+        <div className="true-false-container">
+            <h1>14.</h1>
+            <select>
+                <option value="true">TRUE</option>
+                <option value="false">FALSE</option>
+                <option value="not-given">NOT GIVEN</option>
+            </select>
+            <h1>Heritage Farm is different from most other nearby farms.</h1>
+        </div>
+    );
+}
