5ace4657f3b7e56bcb09f79373a49a8fc4c0d696
Test_repo <22520992@gm.uit.edu.vn>
2024-12-18 00:57:03 +0700

    Mock true false

--- src/components/MultipleChoiceTest/MultipleChoiceTest.tsx
+++ src/components/MultipleChoiceTest/MultipleChoiceTest.tsx
@@ -5,12 +5,14 @@ interface MultipleChoiceTestProps {
   questionOrder: number;
   content: string;
   choiceList: string;
+  answer: string;
 }
 
 export default function MultipleChoiceTest({
   questionOrder,
   content,
   choiceList,
+  answer,
 }: MultipleChoiceTestProps) {
   const options = choiceList.split("\\n");
 
