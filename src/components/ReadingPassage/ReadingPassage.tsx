28cd0d7c69b32f73aadf834734ddd54f806753d1
Test_repo <22520992@gm.uit.edu.vn>
2024-12-17 14:26:53 +0700

    Add Reading passage and multiple choice

---
+++ src/components/ReadingPassage/ReadingPassage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ReadingPassage.css"
+
+export default function ReadingPassage(){
+    return(
+        <div className="reading-section-container">
+            <h1>READING PASSAGE 3</h1>
+            <h3>You should spend about 20 minutes on the reading section below: </h3>
+            <img 
+                src="https://iotcdn.oss-ap-southeast-1.aliyuncs.com/2024-03/Why%20Do%20We%20Touch%20Strangers%20So%20Much-%20A%20History%20Of%20The%20Handshake%20Offers%20Clues%402x.png"
+                alt="section-image">
+            </img>
+            <h2>Title</h2>
+            <p>
+            The long-standing debate about whether to fluoridate our drinking water continues Fluoridation is the addition of fluorine to public water supplies with the aim of reducing tooth decay. The fluorine, when mixed with water, becomes fluoride and the desired concentration of fluoride in public water is approximately one part per million, depending on the regional temperature and hence the amount of water people are likely to drink. Many studies, such as those by McClure in 1970 through to Burt in 1983, have shown that when children drink fluoridated water, their average rate of tooth decay seems greatly reduced. A typical figure claimed is 50 percent reduction. This apparently enormous benefit for children's teeth is the major argument in favor of fluoridation.
+            </p>
+            <p>
+            Three main grounds for opposition to fluoridation have been expressed. First, opponents claim the benefits are exaggerated or not established. Second, there are claims of health risks to pans of the population, for example, allergic reactions. It is also accepted that high levels of fluoride can cause discoloration of otherwise healthy teeth. Proponents do not consider this to be a problem in such small concentrations, whereas opponents disagree - especially because some people drink more water and obtain much more than the standard 1 milligram of fluoride per day. Third, fluoridation is thought to be an infringement on individual rights because it is compulsory medication of all members of a community.
+            </p>
+            <p>
+            An understanding of the fluoridation issue has important implications. If, according to the experts, fluoridation is unquestionably a beneficial and non-hazardous measure, then the wisdom of allowing the public to vote on, and reject it must be questioned.
+            </p>
+        </div>
+    );
+}
--- src/pages/user/StartTestPage.tsx
+++ src/pages/user/StartTestPage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import StartTestHeader from "../../components/StartTestHeader/StartTestHeader.tsx";
+import ReadingSection from "../../components/ReadingPassage/ReadingPassage.tsx";
+import "../../styles/StartTestPage.css"
+import MultipleChoiceTest from "../../components/MultipleChoiceTest/MultipleChoiceTest.tsx";
 
 
 export default function StartTestPage() {
     return (
-        <div>
-            <StartTestHeader></StartTestHeader>
+        <div className="start-page-container">
+            <StartTestHeader/>
+            <div className="content-container">
+                <div className="part">
+                    <ReadingSection></ReadingSection>
+                </div>
+                <div className="part">
+                <MultipleChoiceTest></MultipleChoiceTest>
+                </div>
+            </div>
         </div>
     );
 }
