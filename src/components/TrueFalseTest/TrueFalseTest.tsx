8b6669127695a925a88c0bd281f40c9195c637b9
Test_repo <22520992@gm.uit.edu.vn>
2024-12-18 00:57:03 +0700

    Mock true false

--- src/components/TrueFalseTest/TrueFalseTest.tsx
+++ src/components/TrueFalseTest/TrueFalseTest.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./TrueFalseTest.css"
 
-export default function TrueFalseTest(){
+interface TrueFalseTestProps{
+    questionOrder: number;
+    content: string;
+    answer: string;
+}
+
+export default function TrueFalseTest({questionOrder, content, answer}: TrueFalseTestProps){
     return (
         <div className="true-false-container">
-            <h1>14.</h1>
+            <h1><strong>{questionOrder}.</strong></h1>
             <select>
                 <option value="true">TRUE</option>
                 <option value="false">FALSE</option>
                 <option value="not-given">NOT GIVEN</option>
             </select>
-            <h1>Heritage Farm is different from most other nearby farms.</h1>
+            <h1>{content}</h1>
         </div>
     );
 }
