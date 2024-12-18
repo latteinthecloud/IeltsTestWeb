2a986f22a2c451138c286f4b14617e14562d2d9a
Test_repo <22520992@gm.uit.edu.vn>
2024-12-18 09:49:00 +0700

    Mock matching

--- src/components/MatchingTest/MatchingTest.tsx
+++ src/components/MatchingTest/MatchingTest.tsx
@@ -1,48 +1,33 @@
 import React from "react";
 import "./MatchingTest.css";
 
-export default function MatchingTest() {
+interface MatchingTestProps {
+  questionOrder: number;
+  content: string;
+  choiceList: string;
+  answer: string;
+}
+
+export default function MatchingTest({
+  questionOrder,
+  choiceList,
+  content,
+  answer,
+}: MatchingTestProps) {
+  const options = choiceList.split("\\n");
+
   return (
     <div className="matching-test-container">
-      <table>
-        <thead>
-          <tr>
-            <th colSpan={2}>List of people</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>
-              <strong>A</strong>
-            </td>
-            <td>A content</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>B</strong>
-            </td>
-            <td>B content</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>C</strong>
-            </td>
-            <td>C content</td>
-          </tr>
-        </tbody>
-      </table>
-
       <div className="matching-test-option">
-        <h1>12.</h1>
+        <h1>{questionOrder}.</h1>
         <select>
-          <option>A</option>
-          <option>B</option>
-          <option>C</option>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
-        <h1>
-          The educational system makes students aware of how their memory works.
-        </h1>
+        <h2>{content}</h2>
       </div>
     </div>
   );
