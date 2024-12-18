1fd9cedfea2033425bc6492be4fc838d9489e4f8
Test_repo <22520992@gm.uit.edu.vn>
2024-12-19 01:01:44 +0700

    Complete test footer

--- src/components/StartTestFooter/StartTestFooter.tsx
+++ src/components/StartTestFooter/StartTestFooter.tsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StartTestFooter.css";
 import QuestionPalette from "../../components/QuestionPalette/QuestionPalette.tsx";
 
-export default function StartTestFooter() {
+interface StartTestFooterProps {
+  totalQuestion: number[];
+}
+
+export default function StartTestFooter({
+  totalQuestion,
+}: StartTestFooterProps) {
+  const [activeIndex, setActiveIndex] = useState(1);
+
   return (
     <div className="start-footer-container">
-      <QuestionPalette
-        sectionOrder={1}
-        startQuestion={1}
-        endQuestion={17}
-      ></QuestionPalette>
-      <QuestionPalette
-        sectionOrder={2}
-        startQuestion={18}
-        endQuestion={30}
-      ></QuestionPalette>
-      <QuestionPalette
-        sectionOrder={3}
-        startQuestion={31}
-        endQuestion={40}
-      ></QuestionPalette>
+      {totalQuestion.map((questions, index) => {
+        let startIndex = 0;
+        for (let i = 0; i < index; i++) startIndex += totalQuestion[i];
+
+        return (
+          <QuestionPalette
+            key={index + 1}
+            sectionOrder={index + 1}
+            startQuestion={startIndex + 1}
+            endQuestion={startIndex + questions}
+            onClick={() => setActiveIndex(index + 1)}
+            status={activeIndex === index + 1 ? "active" : ""}
+          ></QuestionPalette>
+        );
+      })}
     </div>
   );
 }
