267a6ae7d52ff67525c8725aa7f731d9379b87bc
Test_repo <22520992@gm.uit.edu.vn>
2024-12-18 15:19:22 +0700

    Add, mock complete question

---
+++ src/components/CompleteTest/CompleteTest.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CompleteTest.css"
+
+interface CompleteTestProps{
+    questionOrder: number;
+    answer: string;
+}
+
+export default function CompleteTest({questionOrder, answer}: CompleteTestProps){
+    
+    return(
+        <input className="complete-test-input" placeholder={questionOrder.toString()}>
+        </input>
+    );
+}
--- src/components/QuestionList/QuestionList.tsx
+++ src/components/QuestionList/QuestionList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CompleteTest from "../CompleteTest/CompleteTest.tsx";
 
 interface QuestionListProps{
     startQuestion: number;
@@ -6,12 +7,15 @@ interface QuestionListProps{
     type: string;
     choiceList?: string;
     img?: string;
+    content?: string;
+    completeAnswer?: string[];
     children?: React.ReactNode;
 }
 
 export default function QuestionList({startQuestion, endQuestion, type,
-    choiceList = "", img = "", children}){
-    const options = choiceList !== null ? choiceList?.split("\\n") : null;
+    content= "",choiceList = "", img = "", completeAnswer= [], children} : QuestionListProps){
+    const options = choiceList !== null ? choiceList.split("\\n") : null;
+    const parts = content !== null ? content.split("\\n"): null;
 
     const containerStyle: React.CSSProperties = {
         display: "flex",
@@ -45,6 +49,12 @@ export default function QuestionList({startQuestion, endQuestion, type,
         padding: "10px",
         textAlign: "left", 
     }
+
+    const formatText: React.CSSProperties={
+        textAlign: "left",
+        fontSize: "15px",
+        margin: "0px",
+    }
     
     return (
         <div style={containerStyle}>
@@ -68,12 +78,12 @@ export default function QuestionList({startQuestion, endQuestion, type,
                         <table style={tableStyle}>
                             <thead>
                                 <tr>
-                                    <th colSpan={2} style={{textAlign: "center"}}>{options[0]}</th>
+                                    <th colSpan={2} style={{textAlign: "center"}}>{content}</th>
                                 </tr>   
                             </thead>
 
                             <tbody>
-                                {options.slice(1).map((option, index) => (
+                                {options.map((option, index) => (
                                     <tr key={index}>
                                         <td style={{width: "20px"}}><strong>{String.fromCharCode(65 + index)}</strong></td>
                                         <td>{option}</td>
@@ -94,14 +104,46 @@ export default function QuestionList({startQuestion, endQuestion, type,
                 </div>
             }
 
-            {type === "complete" &&
+            {type === "complete" && parts !== null &&
                 <div style={containerStyle}>
                     <h2 style={instructionStyle}>Complete the paragraph below.</h2>
                     <h2 style={instructionStyle}>Choose <strong style={{color: "red"}}>ONE WORD ONLY</strong> from the passage for each answer.</h2>
+                    {
+                        parts.map((part, index) => {
+                            let startIndex = 0;
+                            
+                            for(let i = 0; i < index; i++){
+                                startIndex+= countInput(parts[i]);
+                            }
+
+                            return (
+                                <p style={formatText}>{formatPapagraph(part, startQuestion + startIndex, startIndex, completeAnswer)}</p>
+                            );
+                        })
+                    }
                 </div>
             }
 
             {children}
         </div>
     );
+}
+
+
+function formatPapagraph(text: string, startIndex: number, prevIndex: number, completeAnswer: string[]) {
+    const parts = text.split("<i>");
+    return parts.flatMap((part, index) => [
+        <span key={`text-${index}`}>{part}</span>,
+        index < parts.length - 1 && (
+          <CompleteTest key={`input-${index}`} questionOrder={ startIndex + index } answer={completeAnswer[ prevIndex + index]} />
+        ),
+      ]);
+}
+
+function countInput(text: string){
+    return (text.match(/<i>/g) || []).length;
+}
+
+function getQuestionOrder(index: number){
+
 }
