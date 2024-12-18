aed0ea457eb95f019c94fb2b2365fab4a9e47d35
Test_repo <22520992@gm.uit.edu.vn>
2024-12-19 00:23:52 +0700

    Mock footer

--- src/components/QuestionPalette/QuestionPalette.tsx
+++ src/components/QuestionPalette/QuestionPalette.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./QuestionPalette.css";
 
-export default function SectionChange() {
+interface QuestionPaletteProps {
+  sectionOrder: number;
+  startQuestion: number;
+  endQuestion: number;
+}
+
+export default function QuestionPalette({
+  sectionOrder,
+  startQuestion,
+  endQuestion,
+}: QuestionPaletteProps) {
+  const numList = Array.from(
+    { length: endQuestion - startQuestion + 1 },
+    (_, i) => startQuestion + i
+  );
+
   return (
     <div className="question-palette-container">
-      <h1>Part 1</h1>
-      <h2>1</h2>
-      <h2>2</h2>
-      <h2>3</h2>
-      <h2>4</h2>
+      <h1>Part {sectionOrder}</h1>
+      {numList.map((num, index) => {
+        return <h2 key={num}>{num}</h2>;
+      })}
     </div>
   );
 }
