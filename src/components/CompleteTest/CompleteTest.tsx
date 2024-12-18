6fdac9a516bc28a01b52b258aa20606702221448
Test_repo <22520992@gm.uit.edu.vn>
2024-12-18 15:19:22 +0700

    Add, mock complete question

--- src/components/CompleteTest/CompleteTest.tsx
+++ src/components/CompleteTest/CompleteTest.tsx
@@ -1,26 +1,19 @@
 import React from "react";
-import "./CompleteTest.css"
+import "./CompleteTest.css";
 
-interface CompleteTestProps{
-    questionOrder: number;
-    answers: Map<number, string>;
-    handleAnswerChange: (questionNumber: number, answer: string) => void;
+interface CompleteTestProps {
+  questionOrder: number;
+  answer: string;
 }
 
-export default function CompleteTest({questionOrder, answers, handleAnswerChange}: CompleteTestProps){
-    const currentAnswer = answers.get(questionOrder) || "";
-    
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        handleAnswerChange(questionOrder, value);
-    };
-
-    return (
-        <input
-            className="complete-test-input"
-            placeholder={questionOrder.toString()}
-            value={currentAnswer}
-            onChange={handleChange}
-        />
-    );
+export default function CompleteTest({
+  questionOrder,
+  answer,
+}: CompleteTestProps) {
+  return (
+    <input
+      className="complete-test-input"
+      placeholder={questionOrder.toString()}
+    ></input>
+  );
 }
