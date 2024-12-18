3bd6670eeb065549b12cd0393e40e8903c1d3733
Test_repo <22520992@gm.uit.edu.vn>
2024-12-19 01:01:44 +0700

    Complete test footer

--- src/pages/user/StartTestPage.tsx
+++ src/pages/user/StartTestPage.tsx
@@ -144,7 +144,7 @@ export default function StartTestPage() {
         </div>
       </div>
 
-      <StartTestFooter></StartTestFooter>
+      <StartTestFooter totalQuestion={[13, 14, 13]}></StartTestFooter>
     </div>
   );
 }
