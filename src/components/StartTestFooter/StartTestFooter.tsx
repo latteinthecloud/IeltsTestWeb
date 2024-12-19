643852b50f47d9cb4a05a375f8283839693a7673
Test_repo <22520992@gm.uit.edu.vn>
2024-12-19 14:54:04 +0700

    Handle API for reading passage

--- src/components/StartTestFooter/StartTestFooter.tsx
+++ src/components/StartTestFooter/StartTestFooter.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "./StartTestFooter.css";
 import QuestionPalette from "../../components/QuestionPalette/QuestionPalette.tsx";
 
 interface StartTestFooterProps {
   totalQuestion: number[];
+  activeIndex: number;
+  setActiveIndex: (number) => void;
 }
 
 export default function StartTestFooter({
   totalQuestion,
+  activeIndex,
+  setActiveIndex,
 }: StartTestFooterProps) {
-  const [activeIndex, setActiveIndex] = useState(1);
-
   return (
     <div className="start-footer-container">
       {totalQuestion.map((questions, index) => {
