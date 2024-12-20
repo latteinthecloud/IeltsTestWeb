e5bb7fde68cd9bd2b25e3b0226d337e31251c1b8
Test_repo <22520992@gm.uit.edu.vn>
2024-12-20 16:50:48 +0700

    Complete save answer from user

--- src/components/QuestionList/QuestionList.tsx
+++ src/components/QuestionList/QuestionList.tsx
@@ -10,10 +10,12 @@ interface QuestionListProps{
     startQuestion: number;
     endQuestion: number;
     questionList: any;
-    questions: any
+    questions: any;
+    answers: Map<number, string>;
+    handleAnswerChange: (questionNumber: number, answer: string) => void;
 }
 
-export default function QuestionList({startQuestion, endQuestion, questionList, questions} : QuestionListProps){
+export default function QuestionList({startQuestion, endQuestion, questionList, questions, answers, handleAnswerChange} : QuestionListProps){
     const type = questionList.type;
     const [fectString, setFetchString] = useState("");
 
@@ -39,7 +41,8 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
     const containerStyle: React.CSSProperties = {
         display: "flex",
         flexDirection: "column",
-        gap: "15px"
+        gap: "15px",
+        transition: "all 0.3s ease",
     }
 
     const titleStyle: React.CSSProperties = {
@@ -66,7 +69,8 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
         border: "1px solid #000",
         borderRadius: "5px",
         padding: "10px",
-        textAlign: "left", 
+        textAlign: "left",
+        transition: "all 0.3s ease",
     }
 
     const formatText: React.CSSProperties={
@@ -88,8 +92,9 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
                         <MultipleChoiceTest
                             questionOrder={startQuestion + index}
                             content={question.question.content}
-                            choiceList={question.question.choiceList}>
-
+                            choiceList={question.question.choiceList}
+                            handleAnswerChange={handleAnswerChange}
+                            answers={answers}>
                         </MultipleChoiceTest>);
                     })
                 }
@@ -107,7 +112,9 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
                             return (
                                 <TrueFalseTest
                                     questionOrder={startQuestion + index}
-                                    content={question.question.content}>
+                                    content={question.question.content}
+                                    answers={answers}
+                                    handleAnswerChange={handleAnswerChange}>
                                 </TrueFalseTest>
                             );
                         })
@@ -144,7 +151,9 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
                                 <MatchingTest
                                     questionOrder={startQuestion + index}
                                     content={question.question.content}
-                                    optionCount={options.length}>
+                                    optionCount={options.length}
+                                    answers={answers}
+                                    handleAnswerChange={handleAnswerChange}>
                                 </MatchingTest>
                             );
                         })
@@ -157,13 +166,15 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
                 <div style={containerStyle}>
                     <h2 style={instructionStyle}>Complete the labels. Write <strong style={{color: "red"}}>ONE WORD OR A NUMBER</strong> for each answer.</h2>
                     <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-                        <img style={{width: "50%"}} src={fectString} alt="ql-img"></img>
+                        <img style={{width: "50%", transition: "all 0.3s ease"}} src={fectString} alt="ql-img"></img>
                     </div>
                     {
                         questions.map((question, index)=> {
-                            console.log("In a diagram");
                             return(
-                                <DiagramTest questionOrder={startQuestion + index}>
+                                <DiagramTest 
+                                    questionOrder={startQuestion + index}
+                                    answers={answers}
+                                    handleAnswerChange={handleAnswerChange}>
                                 </DiagramTest>
                             );
                         })
@@ -185,7 +196,7 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
                             }
 
                             return (
-                                <p style={formatText}>{formatPapagraph(part, startQuestion + startIndex)}</p>
+                                <p style={formatText}>{formatPapagraph(part, startQuestion + startIndex, answers, handleAnswerChange)}</p>
                             );
                         })
                     }
@@ -196,12 +207,16 @@ export default function QuestionList({startQuestion, endQuestion, questionList, 
 }
 
 
-function formatPapagraph(text: string, startIndex: number) {
+function formatPapagraph(text: string, startIndex: number, answers, handleAnswerChange) {
     const parts = text.split("<i>");
     return parts.flatMap((part, index) => [
         <span key={`text-${index}`}>{part}</span>,
         index < parts.length - 1 && (
-          <CompleteTest key={`input-${index}`} questionOrder={ startIndex + index }/>
+          <CompleteTest 
+            key={`input-${index}`} 
+            questionOrder={ startIndex + index } 
+            answers={answers}
+            handleAnswerChange={handleAnswerChange}/>
         ),
       ]);
 }
--- src/components/SectionComponent/SectionComponent.tsx
+++ src/components/SectionComponent/SectionComponent.tsx
@@ -4,11 +4,13 @@ import QuestionList from "../QuestionList/QuestionList.tsx";
 interface SectionComponentProps{
     questionLists: any;
     lastQuestionIndex: number;
+    answers: Map<number, string>;
+    handleAnswerChange: (questionNumber: number, answer: string) => void;
 }
 
-export default function SectionComponent({questionLists, lastQuestionIndex}: SectionComponentProps){
+export default function SectionComponent({questionLists, lastQuestionIndex, answers, handleAnswerChange}: SectionComponentProps){
     return (
-            <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
+            <div style={{display: "flex", flexDirection: "column", gap: "20px", transition: "all 0.3s ease"}}>
                 {
                     questionLists.map((ql, index)=>{
                         let startIndex = 0;
@@ -22,7 +24,9 @@ export default function SectionComponent({questionLists, lastQuestionIndex}: Sec
                                 startQuestion={lastQuestionIndex + startIndex + 1}
                                 endQuestion={lastQuestionIndex + startIndex + ql.questionList.qnum}
                                 questionList={ql.questionList}
-                                questions={ql.questions}>
+                                questions={ql.questions}
+                                answers={answers}
+                                handleAnswerChange={handleAnswerChange}>
                             </QuestionList>
                         );
                     })
