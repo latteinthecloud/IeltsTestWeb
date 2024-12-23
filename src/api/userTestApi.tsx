40581ac75e7ec0091aab8566e78dcd159fd3c03f
Test_repo <22520992@gm.uit.edu.vn>
2024-12-23 18:01:49 +0700

    Fix UI

--- src/api/userTestApi.tsx
+++ src/api/userTestApi.tsx
@@ -1,14 +1,10 @@
 import axiosClient from "./axiosClient";
 
-const userTestApi ={
-    getAll(accountId: number){
-        const url = "/UserTest/"+ accountId;
-        return axiosClient.get(url);
-    },
-    getById(testId: number){
-        const url = "/UserTest/Info/"+testId;
-        return axiosClient.get(url);
-    }
-}
+const userTestApi = {
+  getAll(accountId: number) {
+    const url = "/UserTest/" + accountId;
+    return axiosClient.get(url);
+  },
+};
 
 export default userTestApi;
